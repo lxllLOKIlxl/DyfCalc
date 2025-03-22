c5ec12f755585ca1e0d74ecf41523190ca2a227d
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-03-22 17:56:38 +0200

    app.py

--- app.py
+++ app.py
@@ -1,34 +1,4 @@
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials
 
-cred = credentials.Certificate({
-    "type": st.secrets["firebase"]["type"],
-    "project_id": st.secrets["firebase"]["project_id"],
-    "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-    "client_email": st.secrets["firebase"]["client_email"],
-    "client_id": st.secrets["firebase"]["client_id"],
-    "auth_uri": st.secrets["firebase"]["auth_uri"],
-    "token_uri": st.secrets["firebase"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
-})
-
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://chatproject-6722b-default-rtdb.firebaseio.com/'
-})
-
-# Функція для тестування підключення до Firebase
-def test_firebase_connection():
-    try:
-        ref = db.reference('test')
-        ref.set({"status": "connected"})
-        st.success("Підключення до Firebase успішне!")
-    except Exception as e:
-        st.error(f"Помилка підключення до Firebase: {e}")
-
-# Інтерфейс додатку Streamlit
-st.title("DyfCalc")
-if st.button("Перевірити підключення до Firebase"):
-    test_firebase_connection()
+# Виводимо секрети, щоб перевірити їх
+st.write(st.secrets["firebase"])
