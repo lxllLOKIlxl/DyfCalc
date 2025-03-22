adc73b5685660cd67bbc607d9f41aedbb52a20f8
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-03-22 19:13:56 +0200

    app.py

--- app.py
+++ app.py
@@ -1,56 +1,24 @@
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, db
 
-# Ініціалізація Firebase
-cred = credentials.Certificate({
-    "type": st.secrets["firebase"]["type"],
-    "project_id": st.secrets["firebase"]["project_id"],
-    "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-    "client_email": st.secrets["firebase"]["client_email"],
-    "client_id": st.secrets["firebase"]["client_id"],
-    "auth_uri": st.secrets["firebase"]["auth_uri"],
-    "token_uri": st.secrets["firebase"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
-})
+# Простий список для збереження повідомлень локально
+chat_history = []
 
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://dyfcalcchat-default-rtdb.firebaseio.com/'
-})
+# Заголовок чату
+st.title("Локальний онлайн-чат")
 
-# Функція для надсилання повідомлень
-def send_message(user, text):
-    ref = db.reference('messages')
-    new_message = {
-        "user": user,
-        "text": text
-    }
-    ref.push(new_message)
-    st.success("Повідомлення надіслано!")
-
-# Функція для отримання повідомлень
-def get_messages():
-    ref = db.reference('messages')
-    messages = ref.get()
-    if messages:
-        return [(msg["user"], msg["text"]) for msg in messages.values()]
-    return []
-
-# Інтерфейс чату
-st.title("Онлайн-чат")
-
+# Інтерфейс користувача
 user = st.text_input("Ваше ім'я")
 message = st.text_input("Введіть повідомлення")
 
+# Надсилання повідомлення
 if st.button("Надіслати"):
     if user and message:
-        send_message(user, message)
+        chat_history.append(f"**{user}:** {message}")
+        st.success("Повідомлення надіслано!")
     else:
         st.warning("Заповніть усі поля!")
 
+# Відображення історії чату
 st.write("### Повідомлення:")
-messages = get_messages()
-for user, text in messages:
-    st.write(f"**{user}:** {text}")
+for msg in chat_history:
+    st.write(msg)
