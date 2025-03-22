f1c174fb789a7afd60f4bb6c16bb9e8fe181b54a
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-03-22 20:15:59 +0200

    app.py

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
-# Ініціалізація Firebase
-cred = credentials.Certificate({
-    "type": st.secrets["firebase"]["type"],
-    "project_id": st.secrets["firebase"]["project_id"],
-    "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-    "client_email": st.secrets["firebase"]["client_email"],
-    "client_id": st.secrets["firebase"]["client_id"],
-    "auth_uri": st.secrets["firebase"]["auth_uri"],
-    "token_uri": st.secrets["firebase"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
-})
-firebase_admin.initialize_app(cred, {
-    'databaseURL': st.secrets["firebase"]["databaseURL"]
-})
+# Перевірка, чи Firebase вже ініціалізований
+if not firebase_admin._apps:
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    })
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': st.secrets["firebase"]["databaseURL"]
+    })
 
 # Функція для надсилання повідомлень
 def send_message(user, text):
